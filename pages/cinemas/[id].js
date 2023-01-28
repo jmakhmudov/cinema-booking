9ef94f4021983fb6b539e3807520025a27689f26
Jaloliddin Makhmudov <makhmudov@tuta.io>
2023-01-28 23:32:44 +0500

    fix empty files

---
+++ pages/cinemas/[id].js
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar"
+import Footer from "../../components/Footer"
+
+export default function Cinema() {
+
+
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    )
+}
